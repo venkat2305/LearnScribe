cd547603c48f282f833c9695c6afc192bab779bd
venkat2305 <heliobvsr2002@gmail.com>
2025-03-08 01:17:30 +0530

    refactor: remove unused imports and state from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='bg-black min-h-screen text-white flex flex-col items-center justify-center space-y-4'>
